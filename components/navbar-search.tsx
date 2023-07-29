145d20da67287c0ba5eb657c06a1181adf811d65
misbahJuwayriyyah <108474345+misbahJuwayriyyah@users.noreply.github.com>
2023-07-29 19:20:17 +0500

    no results page

--- components/navbar-search.tsx
+++ components/navbar-search.tsx
@@ -14,14 +14,16 @@ const NavbarSearch = () => {
   if (!isMounted) {
     return null;
   }
-  const searchResults=()=>{}
-  const changeResults=()=>{}
+  const searchResults = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push("/noResults");
+  }
 
   return ( 
     <div className="ml-auto flex items-center gap-x-4">
-        <form onSubmit={() => searchResults}>
+        <form onSubmit={searchResults}>
         <span className="ml-2 text-sm font-medium text-white">
-          <input type="text" className="ml-2 text-sm font-medium text-black h-7 px-4" placeholder="Search Products in Store ..." onChange={()=>changeResults}></input>
+          <input type="text" className="ml-2 text-sm font-medium text-black h-7 px-4" placeholder="Search Products in Store ..."></input>
         </span>
         </form>
     </div>
