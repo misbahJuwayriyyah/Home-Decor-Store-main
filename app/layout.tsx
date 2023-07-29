9a80994063016c106ca254d11d31921d833c5589
misbahJuwayriyyah <108474345+misbahJuwayriyyah@users.noreply.github.com>
2023-07-29 14:40:27 +0500

    search bar removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import ModalProvider from '@/providers/modal-provider'
 import ToastProvider from '@/providers/toast-provider'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
-import { ChangeEvent,useState,FormEvent } from 'react'
 
 import './globals.css'
 
@@ -15,43 +14,20 @@ export const metadata = {
   description: 'Store - The place for all your purchases.',
 }
 
-interface NavbarProps {
-  searchQuery: string;
-  handleSearchInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  handleSearchFormSubmit: (event: FormEvent<HTMLFormElement>) => void;
-}
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  // Define the necessary state and functions to handle the search query and form submission
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-    // Implement search suggestions here if needed
-  };
-
-  const handleSearchFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Implement search functionality here (e.g., query the database)
-  };
-
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <ToastProvider />
-        <ModalProvider />
-        <Navbar
-          searchQuery={searchQuery}
-          handleSearchInputChange={handleSearchInputChange}
-          handleSearchFormSubmit={handleSearchFormSubmit}
-        />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+      <html lang="en">
+        <body className={font.className}>
+          <ToastProvider />
+          <ModalProvider />
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </html>
+  )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,14 @@
 import Link from "next/link";
+
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
-import { ChangeEvent, FormEvent } from "react";
-import { cn } from "@/lib/utils"
 
-interface NavbarProps {
-  searchQuery: string;
-  handleSearchInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  handleSearchFormSubmit: (event: FormEvent<HTMLFormElement>) => void;
-}
+const Navbar = async () => {
+  const categories = await getCategories();
 
-const Navbar: React.FC<NavbarProps> = async({
-  searchQuery,
-  handleSearchInputChange,
-  handleSearchFormSubmit,
-}) => {
-  const categories = await getCategories(); // You should populate this with actual data from getCategories()
-
-  return (
+  return ( 
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
@@ -27,24 +16,11 @@ const Navbar: React.FC<NavbarProps> = async({
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
-
-          {/* Search bar */}
-          <form onSubmit={handleSearchFormSubmit} className="ml-auto flex items-center gap-x-1">
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchInputChange}
-              placeholder="Search products..."
-              className={cn(
-                'text-sm font-medium transition-colors hover:text-black h-7 px-4',
-              )}
-            />
-          </form>
           <NavbarActions />
         </div>
       </Container>
     </div>
   );
 };
-
+ 
 export default Navbar;
